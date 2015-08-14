6b666c256dd2c46879059d4981b3e16b22680244
Sarfraz <sarfraznawaz2005@gmail.com>
2015-08-14 20:59:14 +0500

    added yii from composer in index file

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
+
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii-1.1.16/framework/yii.php';
+$yii=dirname(__FILE__).'/vendor/yiisoft/yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
